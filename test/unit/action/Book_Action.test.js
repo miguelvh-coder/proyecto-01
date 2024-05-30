101378236ae12da12071914f095a49adc5c48914
miguelvh-coder <72145218+miguelvh-coder@users.noreply.github.com>
2024-05-30 23:23:32 +0000

    a

---
+++ test/unit/action/Book_Action.test.js
@@ -0,0 +1,61 @@
+const Book = require('../models/bookModel');
+
+// Crear un nuevo libro
+exports.createBook = async (req, res) => {
+  try {
+    const book = new Book(req.body);
+    await book.save();
+    res.status(201).send(book);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// Obtener todos los libros
+exports.getBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).send(books);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+// Obtener un libro por ID
+exports.getBookById = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).send();
+    }
+    res.status(200).send(book);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+// Actualizar un libro por ID
+exports.updateBookById = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!book) {
+      return res.status(404).send();
+    }
+    res.status(200).send(book);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// Eliminar un libro por ID
+exports.deleteBookById = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+    if (!book) {
+      return res.status(404).send();
+    }
+    res.status(200).send(book);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
