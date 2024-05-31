a064659dc55f321fa93bb6a0a4d0ff87e37ec6b3
miguelvh-coder <72145218+miguelvh-coder@users.noreply.github.com>
2024-05-31 19:37:29 +0000

    ...

--- test/unit/action/Order_Action.test.js
+++ test/unit/action/Order_Action.test.js
@@ -23,9 +23,40 @@ describe('Pedido unit Actions', () => {
 
         it('debería lanzar un error si la orden no existe', async () => {
             Order.findById.mockResolvedValue(null);
-      
+
             await expect(getOrder('1')).rejects.toThrow('{"code":404,"msg":"Orden inexiste"}');
-          });
+        });
+
+        //para varios pedidos
+        it('debería devolver una lista de pedidos', async () => {
+            const datos = { nombre: 'Nuevo Usuario' };
+            const usuarioCreado = { _id: '1', ...datos };
+            User.create.mockResolvedValue(usuarioCreado);
+
+            const libros1 = [{ _id: '1', titulo: 'Libro 1' }, { _id: '2', titulo: 'Libro 2' }];
+            const libros2 = [{ _id: '1', titulo: 'a' }, { _id: '2', titulo: 'patata' }];
+            const libros3 = [{ _id: '1', titulo: 'e' }, { _id: '2', titulo: 'cursed' }];
+            const pedidos = [
+                { _id: '1', libros_ids: libros1, estado: "en progreso", direccion_envio: "aca" },
+                { _id: '2', libros_ids: libros2, estado: "en progreso", direccion_envio: "uninorte" },
+                { _id: '3', libros_ids: libros3, estado: "en progreso", direccion_envio: "alla" },
+            ];
+            User.find.mockResolvedValue(pedidos);
+
+            const results = await getOrders();
+
+            expect(results).toEqual(pedidos);
+        });
+
+        it('debería devolver una lista vacía si no hay pedidos', async () => {
+            Order.find.mockResolvedValue([]);
+
+            const result = await getOrders();
+
+            expect(result).toEqual([]);
+        });
+
+
 
     });
 
